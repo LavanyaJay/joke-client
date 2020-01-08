86f78e3bc6ad6ac0ee6d1dd1103492c139f13369
LavanyaJay <lavanyajay75@gmail.com>
2020-01-08 13:44:02 +0100

    Sharing in twitter

--- src/actions/joke.js
+++ src/actions/joke.js
@@ -1,11 +1,10 @@
-import jokeStore from "../stores/joke";
+import jokeStore from "../stores/joke.js";
 
 export function getJoke(id) {
-  console.log("in action" + id);
-  //const id = Math.floor(Math.random() * (3 - 1 + 1)) + 1;
   fetch(`http://127.0.0.1:8000/api/jokes/${id}`)
     .then(res => res.json())
     .then(data => {
+      console.log(data);
       jokeStore.joke = data;
     });
   return id;
